1f74612933af3951547ac13ce3b209dfd96b63dc
LucasNadalin <lucasnfrancisco@gmail.com>
2025-12-11 21:07:06 -0300

    LojaEspricio

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {clienteController} = require("../controllers/clienteController");
 const {authController} = require("../controllers/authController");
+const {verify} = require("../middlewares/authMiddleware")
 
 router.post("/clientes/login", authController.clienteLogin);
 
-router.get("/clientes", clienteController.mostrarClientes);
+router.get("/clientes", verify.cliente, clienteController.mostrarClientes);
 
 router.post("/clientes", clienteController.cadastrarCliente);
 
